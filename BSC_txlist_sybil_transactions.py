c041744273d38e3492bd107b62b6b8e8a55396db
amyeranly <143498880+amyeranly@users.noreply.github.com>
2023-08-31 16:12:00 +0800

    Update BSC_txlist_sybil_transactions.py

--- BSC_txlist_sybil_transactions.py
+++ BSC_txlist_sybil_transactions.py
@@ -59,7 +59,7 @@ for index, group in enumerate(groups, start=1):
             # Add current transaction hash to the seen_hashes set
             seen_hashes.add(tx["hash"])
             if tx["from"] in addresses and tx["to"] in addresses and tx["from"] != tx["to"]:
-                if tx["input"].startswith("0x"):
+                if tx["input"] == "0x":
                     transfer_str = (f'hash: "{tx["hash"]}" from: "{tx["from"]}" to: "{tx["to"]}" '
                                     f'tokenSymbol: "ETH" value: "{int(tx["value"]) / (10 ** 18)}"')
                     group_token_transfers[index].append(transfer_str)
